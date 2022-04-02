332345518051e3a68b91a77c2afa79893e1276db
Syd <syd@gmail.com>
2022-04-02 11:07:18 +0800

    First commit.

---
+++ app/src/main/java/syd/example/recitingwords/MainActivity.kt
@@ -0,0 +1,11 @@
+package syd.example.recitingwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
